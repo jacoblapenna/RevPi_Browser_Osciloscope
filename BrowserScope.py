d3eae0fcb6eb80da3f542ec2a64930fcddc068f0
Jacob Lapenna <jlapenna@usbr.gov>
2022-04-29 14:39:57 -0600

    Trying to add mcc haqhat.

--- BrowserScope.py
+++ BrowserScope.py
@@ -143,6 +143,7 @@ class DataStreamer:
             while True:
                 if stream_data:
                     buffer += self._daq.a_in_scan_read(-1, 0).data
+                    print(buffer)
                 if self._producer.poll():
                     instruction = self._producer.recv()
                     if instruction == "start_stream":
