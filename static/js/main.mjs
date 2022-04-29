5e549207cf2ed5c16a1d9208d7b69536dd205982
Jacob Lapenna <jlapenna@usbr.gov>
2022-04-29 15:03:03 -0600

    Trying to add mcc haqhat.

--- static/js/main.mjs
+++ static/js/main.mjs
@@ -38,6 +38,8 @@ socket.on("stream_stopped", function() {
 });
 
 socket.on("new_data", function(data) {
+  console.log(deque.length);
+  
   data.data.forEach( function (value) {
     deque.push(value)
   });
