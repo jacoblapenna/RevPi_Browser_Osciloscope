e7f3932d182ea13d072b730aef2b7d0057c45c7d
Jacob Lapenna <jlapenna@usbr.gov>
2022-05-03 09:43:15 -0600

    DataStreamer debug.

--- DataStreamer.py
+++ DataStreamer.py
@@ -11,69 +11,69 @@ class DataStreamer:
     pull from AIO on revpi here when ready
     """
     def __init__(self):
-        self._produce_stream = False # TEMP
+        # self._produce_stream = False # TEMP
         self._producer_socketio = SocketIO(message_queue='redis://')
         self._controller_conn, self._producer_conn = Pipe()
         self._producer_process = Process(target=self._produce, name="producer_process")
         self._producer_process.start()
 
     def _produce(self):
-        while True:
-            if self._produce_stream:
-                new_data = round(randint(-500, 500)/100, 2)
-                self._producer_socketio.emit("new_data", {"data" : new_data}) # emit here
-            if self._producer_conn.poll():
-                instruction = self._producer_conn.recv()
-                if instruction == "start_stream":
-                    self._produce_stream = True
-                    self._producer_socketio.emit("stream_started")
-                elif instruction == "stop_stream":
-                    self._produce_stream = False
-                    self._producer_socketio.emit("stream_stopped")
-                else:
-                    raise Exception("Invalid instruction!")
-            sleep(0.1)
-        # """
-        # This method is ran in another process, and, to be thread safe, all
-        # class attributes used here cannot be used in any other method other
-        # than __init__.
-        #
-        # This process is always running and whether data is streamed or not is
-        # determined via duplex communication through the pipe.
-        #
-        # Eventually, an instruction will need to  be added to stop and restart
-        # the pipe and process. This should be done whenever control is taken
-        # and given up.
-        # """
-        # class DAQ:
-        #     def __init__(self, socketio, conn):
-        #         self._produce_stream = False
-        #         self._socketio = socketio
-        #         self._conn = conn
-        #         self._daq = revpimodio2.RevPiModIO(autorefresh=True)
-        #
-        #     def _cycle_handler(self, ct):
-        #         if self._produce_stream:
-        #             new_data = self._daq.io.InputValue_1.value/1000
-        #             self._socketio.emit("new_data", {"data" : 0.00})
-        #         if self._conn.poll():
-        #             instruction = self._conn.recv()
-        #             if instruction == "start_stream":
-        #                 self._produce_stream = True
-        #                 self._socketio.emit("stream_started")
-        #             elif instruction == "stop_stream":
-        #                 self._produce_stream = False
-        #                 self._socketio.emit("stream_stopped")
-        #             else:
-        #                 raise Exception(f"Producer received invalid instruction: instruction={instruction}")
-        #
-        #     def produce(self):
-        #         self._daq.cycleloop(self._cycle_handler, cycletime=25)
-        #
-        # socketio = self._producer_socketio
-        # conn = self._producer_conn
-        # daq = DAQ(socketio, conn)
-        # daq._cycle_handler(1)
+        # while True:
+        #     if self._produce_stream:
+        #         new_data = round(randint(-500, 500)/100, 2)
+        #         self._producer_socketio.emit("new_data", {"data" : new_data}) # emit here
+        #     if self._producer_conn.poll():
+        #         instruction = self._producer_conn.recv()
+        #         if instruction == "start_stream":
+        #             self._produce_stream = True
+        #             self._producer_socketio.emit("stream_started")
+        #         elif instruction == "stop_stream":
+        #             self._produce_stream = False
+        #             self._producer_socketio.emit("stream_stopped")
+        #         else:
+        #             raise Exception("Invalid instruction!")
+        #     sleep(0.1)
+        """
+        This method is ran in another process, and, to be thread safe, all
+        class attributes used here cannot be used in any other method other
+        than __init__.
+
+        This process is always running and whether data is streamed or not is
+        determined via duplex communication through the pipe.
+
+        Eventually, an instruction will need to  be added to stop and restart
+        the pipe and process. This should be done whenever control is taken
+        and given up.
+        """
+        class DAQ:
+            def __init__(self, socketio, conn):
+                self._produce_stream = False
+                self._socketio = socketio
+                self._conn = conn
+                self._daq = revpimodio2.RevPiModIO(autorefresh=True)
+
+            def _cycle_handler(self, ct):
+                if self._produce_stream:
+                    new_data = self._daq.io.InputValue_1.value/1000
+                    self._socketio.emit("new_data", {"data" : 0.00})
+                if self._conn.poll():
+                    instruction = self._conn.recv()
+                    if instruction == "start_stream":
+                        self._produce_stream = True
+                        self._socketio.emit("stream_started")
+                    elif instruction == "stop_stream":
+                        self._produce_stream = False
+                        self._socketio.emit("stream_stopped")
+                    else:
+                        raise Exception(f"Producer received invalid instruction: instruction={instruction}")
+
+            def produce(self):
+                self._daq.cycleloop(self._cycle_handler, cycletime=25)
+
+        socketio = self._producer_socketio
+        conn = self._producer_conn
+        daq = DAQ(socketio, conn)
+        daq.produce()
 
     def control_stream(self, instruction):
         if instruction == "start_stream":
